f675ad0a88f463e87118c4ab3b9140760ff2c581
youssefry01 <youssefry01@gmail.com>
2025-07-30 22:56:45 +0300

    Added description

--- frontend/src/components/Footer/Copyright.js
+++ frontend/src/components/Footer/Copyright.js
@@ -7,7 +7,7 @@ const Copyright = () => {
 
     <div className="flex w-full justify-center items-center border-black border-opacity-10 border-t p-2 dark:border-white dark:border-opacity-10">
 
-      <p className="text-black font-JosefinSans text-lg font-normal dark:text-white"><u>Ustore</u> <span className="text-black text-opacity-60 dark:text-white dark:text-opacity-60"> &copy; {year} Powered By</span> us0f.</p>
+      <p className="text-black font-JosefinSans text-lg font-normal dark:text-white"><u>Ustore</u> <span className="text-black text-opacity-60 dark:text-white dark:text-opacity-60"> &copy; {year} Powered By</span> Youssef Ramadan.</p>
 
     </div>
   )
