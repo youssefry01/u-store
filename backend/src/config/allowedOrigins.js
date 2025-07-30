ab1b097f313ba6b0c227690ebdad529abfe9a633
youssefry01 <youssefry01@gmail.com>
2025-07-30 22:42:24 +0300

    Update CORS settings and fix logger middleware

--- backend/src/config/allowedOrigins.js
+++ backend/src/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://www.us0f.com',
+    'https://u-store-jet.vercel.app',
     'http://127.0.0.1:5500',
     'https://events-am.gw.postman.com/',
     'http://localhost:3000',
--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3500/',
+    // baseUrl: 'http://localhost:3500/',
+    baseUrl: 'https://u-store-backend.onrender.com/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
